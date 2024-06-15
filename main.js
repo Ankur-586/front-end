6be0398c93bfe99d6e56f0e38b7ff8f5e546c25a
Ankur586 <ankur.singh586@gmail.com>
2024-06-15 13:42:01 +0530

    main.js

---
+++ main.js
@@ -0,0 +1,38 @@
+/* 
+Note:
+The var keyword was used in all JavaScript code from 1995 to 2015.
+The let and const keywords were added to JavaScript in 2015.
+
+Var Keyword:
+The var keyword should only be used in code written for older browsers.
+Variables declared with let cannot be Redeclared in the same scope
+
+
+*/
+
+
+let firstName = "John",
+lastName = "Doe",
+age = 35;
+
+// alert(10 + 5);
+// alert(10 / 2);
+// alert(15 % 9);
+
+// 1. Write a JavaScript function to calculate the sum of two numbers. 
+function test(a,b) {
+    return a + b
+}
+let res = test(2,2)
+console.log(res)
+
+// 2. Write a JavaScript program to find the maximum number in an array. 
+// console.log(arr.sort(function(arr){return arr[-1]}))
+
+function max_return(arr) {
+    arr.sort() 
+    return arr[arr.lenght - 1]
+}
+const a = [3,4,2,1]
+let max_res = max_return(a)
+console.log(max_res)
---
+++ test.py
@@ -0,0 +1,8 @@
+import pywhatkit
+ 
+ 
+pywhatkit.sendwhatmsg("+918884154409", 
+                      "Geeks For Geeks!", 
+                      18, 30)
+
+# 7311149472
